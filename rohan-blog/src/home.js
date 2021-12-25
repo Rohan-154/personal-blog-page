2848f8584e8b2ee4c3aa9e38a8ab2bfe47f1e5d2
Rohan-154 <86555869+Rohan-154@users.noreply.github.com>
2021-12-25 21:14:23 +0530

    initial

--- rohan-blog/src/home.js
+++ rohan-blog/src/home.js
@@ -1,13 +1,19 @@
+import {useState} from 'react';
+import BlogList from './BlogList';
 const Home = () => {
-    const handleClick= ()=>{
-        console.log("hello ninjas");
-    }
-    return (
-        <div className="home">
-            <h2>Homepage</h2>
-            <button onClick={handleClick}> Click Me </button>
-        </div>
-      );
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+      ])
+   
+
+  return (
+    <div className="home">
+       <BlogList blogs={blogs} title="All Blogs"/>
+       <BlogList blogs={blogs.filter((blog)=> blog.author=== "mario")} title="Mario's blogs"/>
+    </div>
+  );
 }
- 
+
 export default Home;
