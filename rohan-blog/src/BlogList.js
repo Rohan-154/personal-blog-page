615c2b26919de8ad3dc18d1a2c28ef96844283d6
Rohan-154 <86555869+Rohan-154@users.noreply.github.com>
2021-12-26 16:54:45 +0530

    initial

--- rohan-blog/src/BlogList.js
+++ rohan-blog/src/BlogList.js
@@ -1,17 +1,25 @@
 const BlogList = (props) => {
-    const blogs= props.blogs;
-    const title= props.title;
-    return (
-        <div className="blog-list">
-            <h1>{title}</h1>
-             {blogs.map(blog=>(
-            <div className="blog-preview">
-                <h2>{blog.title}</h2>
-                <p>written by{blog.author}</p>
-            </div>
-        ))}
+  const blogs = props.blogs;
+  const title = props.title;
+  const handleDelete = props.handleDelete;
+  return (
+    <div className="blog-list">
+      <h1>{title}</h1>
+      {blogs.map((blog) => (
+        <div className="blog-preview">
+          <h2>{blog.title}</h2>
+          <p>written by{blog.author}</p>
+          <button
+            onClick={() => {
+              handleDelete(blog.id);
+            }}
+          >
+            delete blog
+          </button>
         </div>
-      );
-}
- 
+      ))}
+    </div>
+  );
+};
+
 export default BlogList;
