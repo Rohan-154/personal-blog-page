25ee1cfe5aa020b2b5e761afb1cb69fa5035d818
Rohan-154 <86555869+Rohan-154@users.noreply.github.com>
2021-12-27 14:15:09 +0530

    initial

--- rohan-blog/src/BlogList.js
+++ rohan-blog/src/BlogList.js
@@ -6,16 +6,9 @@ const BlogList = (props) => {
     <div className="blog-list">
       <h1>{title}</h1>
       {blogs.map((blog) => (
-        <div className="blog-preview">
+        <div className="blog-preview" key={blog.id}>
           <h2>{blog.title}</h2>
-          <p>written by{blog.author}</p>
-          <button
-            onClick={() => {
-              handleDelete(blog.id);
-            }}
-          >
-            delete blog
-          </button>
+          <p>written by {blog.author}</p>
         </div>
       ))}
     </div>
---
+++ rohan-blog/src/usefetch.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+
+const useFetch= (url)=>{
+    const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const[error, setError]= useState(null);
+    useEffect(() => {
+        setTimeout(() => {
+          fetch(url)
+            .then((res) => {
+              if(!res.ok){
+                throw Error("could not fetch the data for the resource")
+              }
+              return res.json();
+            })
+            .then((data) => {
+              setData(data);
+              setIsPending(false);
+              setError(null);
+            })
+            .catch((error)=>{
+              setError(error.message);
+              setIsPending(false)
+            })
+        },500);
+      }, [url]);
+      return{data, isPending, error}
+}
+export default useFetch;
